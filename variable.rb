515a64f60e2a6b8b257b0e64ecc1fc1a71d21399
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2019-12-14 18:25:35 +0000

    Done.

--- variable.rb
+++ variable.rb
@@ -1,3 +1,11 @@
  # Define a local variable called 'greeting',
  # give it the value of the string "Hello World"
 greeting = "Hello World"
+first_number= 7
+second_number= 14
+
+sum = first_number + second_number
+
+puts sum
+current_president = "Barack Obama"
+puts "Im 2016, the #{current_president}."
