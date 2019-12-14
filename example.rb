3c48d53c87f2f4ec680b8c56d2db23e3c744970a
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2019-12-14 20:54:26 +0000

    Automatically backed up by Learn

--- example.rb
+++ example.rb
@@ -1,13 +1,5 @@
-x = 3
-y = 20
-
-if x <20 && x. even?
-    if x < 5 || x. odd?
-    elsif x == 3
-        puts "this is a magic number"
-      else
-        puts "nothing
-      end
-    else
-      puts "not less than 20, and not even"
-    end
+array = []
+100.times do |i|
+    array.push(i+1)
+end
+puts array 
