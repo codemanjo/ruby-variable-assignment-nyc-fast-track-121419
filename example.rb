4f0287beed66d4838c2ef506777725d435a97b08
JOSIAH E MODESTE <ephraimmo2297@gmail.com>
2019-12-14 20:19:50 +0000

    Automatically backed up by Learn

---
+++ example.rb
@@ -0,0 +1,13 @@
+x = 3
+y = 20
+
+if x <20 && x. even?
+    if x < 5 || x. odd?
+    elsif x == 3
+        puts "this is a magic number"
+      else
+        puts "nothing
+      end
+    else
+      puts "not less than 20, and not even"
+    end
--- variable.rb
+++ variable.rb
@@ -7,5 +7,5 @@ second_number= 14
 sum = first_number + second_number
 
 puts sum
-current_president = "Barack Obama"
+current_president = "stephen colbert"
 puts "Im 2016, the #{current_president}."
